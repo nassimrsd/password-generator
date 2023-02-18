64f27517a26c74a06d1501e6fb2e96970f4f3f8e
nassimrsd <nassimeljattari03@gmail.com>
2023-02-18 01:13:06 +0100

    adding print command to print password

--- passwordgen.py
+++ passwordgen.py
@@ -3,4 +3,4 @@ import random
 lowerCase = "abcdefghijklmnopqrstuvwxyz"
 upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "0123456789"
-symbols = "!@#$%^&*()."
+symbols = "!@#$%^&*()."symbols = "!@#$%^&*()."
