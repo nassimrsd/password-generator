8fba2429ba8e8548980aeb819087b62ee4a7aa65
nassimrsd <nassimeljattari03@gmail.com>
2023-02-18 02:22:59 +0100

    updated passwordgen.py

--- passwordgen.py
+++ passwordgen.py
@@ -1,14 +1,21 @@
 import random
+import os
 
 lowerCase = "abcdefghijklmnopqrstuvwxyz"
 upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "0123456789"
 symbols = "!@#$%^&*()."
+i = ""
+while i != "x":
+    os.system('clear')
+    string = lowerCase + upperCase + numbers + symbols
+    length = int(input("How many characters should your password contain?  (More than 8 characters is recommended)\n"))
+    password = "".join(random.sample(string,length))
+    print("password:\n" + password)
 
-string = lowerCase + upperCase + numbers + symbols
 
-length = int(input("how many characters should your password contain?  (More than 8 characters is recommended)\n"))
+    i = input("Enter 'x' to quit")
 
-password = "".join(random.sample(string,length))
-
-print("password:\n" + password)
+    
+    
+    
